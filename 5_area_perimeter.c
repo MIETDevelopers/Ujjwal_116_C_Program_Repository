7466ae79b48c8a4d6bc591ee95e071e8dbddbc74
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2021-03-18 20:52:40 +0530

    AREA AND PERIMETER OF CIRCLE, SQUARE AND RECTANGLE

--- 5_area_perimeter.c
+++ 5_area_perimeter.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
+/*NAME:UJJWAL SHARMA
+PURPOSE:TO FIND THE AREA AND PERIMETER OF CIRCLE, SQUARE AND RECTANGLE*/
+#include <stdio.h>	//PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD INPUT OUTPUT HEADER FILE
 
-int main()
+int main()	//STARTING OF MAIN PROGRAM
 {
-	float radiusofcircle, sideofsquare, lengthofrectangle, bredthofrectangle;
+	float radiusofcircle, sideofsquare, lengthofrectangle, bredthofrectangle;	/*DECLARING
+	VARIABLES NAMED "radiusofcircle", "sideofsquare", "lengthofrectangle", "bredthofrectangle" */
 	scanf("%f %f %f %f", &radiusofcircle, &sideofsquare, &lengthofrectangle, &bredthofrectangle);
-	printf("\nArea of Circle =%f ",3.14*radiusofcircle*radiusofcircle);
-	printf("\nPerimeter of Circle =%f ",2*3.14*radiusofcircle);
-	printf("\nArea of Square = %f",sideofsquare*sideofsquare);
-	printf("\nPerimeter of Square =%f",4*sideofsquare);
-	printf("\nArea of Rectangle =%f",lengthofrectangle*bredthofrectangle);
-	printf("\nPerimeter of Rectangle =%f",2*(lengthofrectangle+bredthofrectangle));
-	return 0;
+	/* READ radiusofcircle, sideofsquare, lengthofractangle, bredthofrectangle */
+	printf("\nArea of Circle =%f ",3.14*radiusofcircle*radiusofcircle);	//PRINT AREA OF CIRCLE
+	printf("\nPerimeter of Circle =%f ",2*3.14*radiusofcircle);	//PRINT PERIMETER OF CIRCLE
+	printf("\nArea of Square = %f",sideofsquare*sideofsquare);	//PRINT AREA OF SQUARE
+	printf("\nPerimeter of Square =%f",4*sideofsquare);	//PRINT PERIMETER OF SQUARE
+	printf("\nArea of Rectangle =%f",lengthofrectangle*bredthofrectangle);	//PRINT AREA OF RECTANGLE
+	printf("\nPerimeter of Rectangle =%f",2*(lengthofrectangle+bredthofrectangle));	//PRINT PERIMETER OF RECTANGLE
+	return 0;	//RETURN STATEMENT
 }
