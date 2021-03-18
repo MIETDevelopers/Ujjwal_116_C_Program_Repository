4274ec6ee7f6b9d0c1b37d87856beddbfbf3674d
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2021-03-18 19:47:09 +0530

    Update 10_fact.c

--- 10_fact.c
+++ 10_fact.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-int main(){
-	int num, fact=1, i;
-	scanf("%d",&num);
-	printf("Enter a number = %d",num);
-	for (i=num;i>=1;i--){
+/*NAME:UJJWAL SHARMA
+PURPOSE:TO FIND THE FACTORIAL OF A NUMBER USING FOR LOOP*/
+#include<stdio.h>//PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD LIBRARY INPUT OUTPUT HEADER FILE
+int main(){		//STARTING OF MAIN PROGRAM
+	int num, fact=1, i;		//DECLARING VARIABLES NAMED "num", "fact", AND "i"
+	scanf("%d",&num);		//READ THE USER INPUT
+	printf("Enter a number = %d",num);		//PRINT THE INPUT
+	for (i=num;i>=1;i--){		//STARTING FOR LOOP
 		fact = fact*i;
 	}
-	printf("\nFactorial of a number %d is = %d", num, fact);
-	return 0;
+	printf("\nFactorial of a number %d is = %d", num, fact);	//PRINT THE FACTORIAL OF A NUMBER
+	return 0;		//RETURN STATEMENT
 }
