189e3b54de5b2bc939ac101f47b5f5e6ebf5ce90
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2021-03-18 20:22:19 +0530

    ARITHMETIC OPERATIONS USING ADDITION, SUBTRACTION, MULTIPLICATION AND REMAINDER OPERATORS

--- 4_operation.c
+++ 4_operation.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
-int main()
+/*NAME:UJJWAL SHARMA
+PURPOSE:ARITHMETIC OPERATIONS USING ADDITION, SUBTRACTION, MULTIPLICATION AND REMAINDER OPERATORS*/
+#include <stdio.h>//PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD INPUT OUTPUT HEADER FILE
+int main()  //STARTING OF MAIN PROGRAM
 {
-   int num1, num2, add, subtract, multiply;
-   float divide;
+   int num1, num2, add, subtract, multiply;  //DECLARING THE VARIABLES NAME "num1", "num2", "add", "subtract", "multiply"
+   float divide;  //AND "divide" 
  
-   scanf("%d%d", &num1, &num2);
-   printf("Enter two integers = %d %d\n", num1, num2);
+   scanf("%d%d", &num1, &num2);  //READ "num1" AND "num2"
+   printf("Enter two integers = %d %d\n", num1, num2);   //PRINT THE VALUE OF "num1" AND "num2"
  
-   add = num1 + num2;
-   subtract = num1 - num2;
-   multiply = num1 * num2;
-   divide = num1 / (float)num2;   //typecasting, you can also write: divide = (float)first/second
+   add = num1 + num2;   //ADDITION FORMULA
+   subtract = num1 - num2; //SUBTRACTION FORMULA
+   multiply = num1 * num2; //MULTIPLICATION FORMULA
+   divide = num1 / (float)num2;   //DIVISION FORMULA AND TYPECATING, YOU CAN ALSO WRITE: divide = (float)num1/num2
 
-   printf("Sum = %d\n", add);
-   printf("Difference = %d\n", subtract);
-   printf("Multiplication = %d\n", multiply);
-   printf("Division = %.2f\n", divide); // "%.2lf" to print two decimal digits, by default (%lf) we get six
+   printf("Sum = %d\n", add);    //PRINT SUM OF TWO NUMBER
+   printf("Difference = %d\n", subtract);    //PRINT SUBTRACTION OF TWO NUMBER
+   printf("Multiplication = %d\n", multiply);   //PRINT MULTIPLICATION OF TWO NUMBER
+   printf("Division = %.2f\n", divide); //PRINT DIVISION OF TWO NUMBER AND "%.2lf" TO PRINT TWO DECIMAL DIGITS, BY DEFAULT (%lf) WE GET SIX
  
-   return 0;
+   return 0;   //RETURN STATEMENT
 }
