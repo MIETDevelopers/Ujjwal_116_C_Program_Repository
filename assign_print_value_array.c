aa1c456b39545a405b265f5b91403a08be32f3f6
Ujjwal Sharma <2020a1r116@mietjammu.in>
2021-03-31 12:24:51 +0530

    DEMONSTRATE AN ARRAY OF 10 INTEGER INITIALIZE AND PRINTING THIER VALUE

---
+++ assign_print_value_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int values[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  printf("Displaying integers: \n");
+
+  // printing elements of an array
+  for(int i = 0; i < 10; ++i) {
+    printf("%d\n", values[i]);
+  }
+  return 0;
+}
