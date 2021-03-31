dc0ef4505ab463895b40b4d65b87d996bed604d7
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2021-03-31 15:05:33 +0530

    TO DEMONSTRATE THE USE OF STRING OPERATOR

--- 21_string_operator.c
+++ 21_string_operator.c
@@ -3,7 +3,7 @@ PURPOSE:TO DEMONSTRATE THE USE OF STRING FUNCTIONS*/
 #include <stdio.h>  //PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD INPUT OUTPUT HEADER FILE
 #include <string.h> //PREPROSESSIVE DIRECTIVE TO INCLUDE STRING INPUT OUTPUT HEADER FILE
 int main()  //STARING OF MAIN PROGRAM
-{
+{    //STRING OPERATOR - strlen()
      char str1[20] = "Ujjwal_sharma";
      printf("Length of string str1 when maxlen is 30: %d", strnlen(str1, 30));
      printf("Length of string str1 when maxlen is 10: %d", strnlen(str1, 10));
