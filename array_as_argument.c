e00fbf2eaa22baa4846064f334e460911bd129ec
Ujjwal Sharma <2020a1r116@mietjammu.in>
2021-03-31 16:53:03 +0530

    FUNCTION TO TAKE ARRAY AS ARGUMENT

---
+++ array_as_argument.c
@@ -0,0 +1,13 @@
+/*NAME:UJJWAL SHARMA
+PURPOSE:TO FUNCTION TO TAKE ARRAY AS ARGUMENT*/
+#include <stdio.h>  //PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD INPUT OUTPUT HEADER FILE
+int fnToTakeArrAsArgs(int x[]);		//FUNCTION INITIALIZATION
+int main(){		//STARTING OF MAIN PROGRAM
+  int arr[3]={45,55,65};	//DECLARING ARRAY
+  printf("Hee\n");
+  fnToTakeArrAsArgs(arr);	//FUNCTION CALLING
+  return 0;		//RETURN STATEMENT
+}
+int fnToTakeArrAsArgs(int x[]){		//FUNCTION DEFINITION
+  printf("X=%d",x[2]);
+}
